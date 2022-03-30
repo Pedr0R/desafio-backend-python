8f0a93c95038c8477df7db6068815e10205764eb
Pedro Ramos <pedroramos327698@gmail.com>
2022-03-30 19:34:15 -0300

    implementing flights

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-from email import message
-import http.server
-import socketserver
+def flights():
+    """
+    List all flights.
+    """
+    for i in range(len(flights)):
+        print(f"{i}: {flights[i]}")
 
-PORT = 3333
+flights = ['Flight 1', 'Flight 2', 'Flight 3']
 
-class Handler(http.server.simple_http_server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        message = "Hello World"
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(message.encode("utf8"))
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+while True:
+  input('insert your command')
+  if input == '/list':
+    flights()
